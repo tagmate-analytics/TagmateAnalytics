4249f929e1a0251a75471f3c76ab8f3e18209ab5
kuldeep-IT <rathod99244@gmail.com>
2024-07-26 15:56:43 +0530

    api implemented

--- firebaseButton/src/main/java/com/tagmate/RemoteConfigSDK.java
+++ firebaseButton/src/main/java/com/tagmate/RemoteConfigSDK.java
@@ -2,6 +2,12 @@ package com.tagmate;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
 
 public class RemoteConfigSDK {
     private static final String PREFS_NAME = "RemoteConfigPrefs";
@@ -23,10 +29,47 @@ public class RemoteConfigSDK {
     }
 
     public void fetchRemoteConfig() {
-        // Mocking remote fetch. In real implementation, make a network request here.
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(BUTTON_TEXT_KEY, DEFAULT_BUTTON_TEXT);
-        editor.apply();
+        new FetchRemoteConfigTask().execute("http://192.168.2.162:3000/api/getButtonText");
+    }
+
+    private class FetchRemoteConfigTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            if (response != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String buttonText = jsonObject.getString("buttonText");
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(BUTTON_TEXT_KEY, buttonText);
+                    editor.apply();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public String getButtonText() {
